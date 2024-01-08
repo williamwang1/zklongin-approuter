81508a2aa6c82a308a8d417bebe90a5971b4335f
WANG LEI <william@huahai.sg>
2024-01-08 18:27:50 +0800

    first commit

---
+++ src/app/api/auth/[...auth]/route.ts
@@ -0,0 +1,25 @@
+'use client'
+import { sui, zkp, zkw } from "@/lib/api/shinami";
+import {
+  FACEBOOK_CLIENT_ID,
+  GOOGLE_CLIENT_ID,
+  TWITCH_CLIENT_ID,
+} from "@/lib/shared/openid";
+import { authHandler } from "@shinami/nextjs-zklogin/server/pages";
+
+// This handler should be installed at route "/api/auth/[...api]".
+// If you need to use a different path, set env NEXT_PUBLIC_AUTH_API_BASE to override the default.
+function handler() {
+    authHandler(
+        sui, // Alternatively, you can use mystenSui
+        zkw, // Alternatively, you can use mystenSaltProvider
+        zkp, // Alternatively, you can use mystenProver
+        {
+          google: GOOGLE_CLIENT_ID ? [GOOGLE_CLIENT_ID] : undefined,
+          facebook: FACEBOOK_CLIENT_ID ? [FACEBOOK_CLIENT_ID] : undefined,
+          twitch: TWITCH_CLIENT_ID ? [TWITCH_CLIENT_ID] : undefined,
+        }
+    )
+};
+
+export { handler as POST, handler as GET };
